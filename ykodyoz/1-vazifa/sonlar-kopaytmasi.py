5129abf13b0f9928cbfc7c28182c5b0eb6d9026d
Bobur <boburotaboyev0@gmail.com>
2026-07-03 22:08:09 +0500

    Refactor productExceptSelf function to handle edge cases and improve readability

--- ykodyoz/1-vazifa/sonlar-kopaytmasi.py
+++ ykodyoz/1-vazifa/sonlar-kopaytmasi.py
@@ -1,27 +1,32 @@
 def productExceptSelf(nums: list) -> list:
+    if len(nums) == 0:
+        return []
+    if len(nums) == 1:
+        return [1]
+
     _result_left = []
     _lastNum = 1
     for num in nums:
         _lastNum *= num
         _result_left.append(_lastNum)
 
-    _result_right = []
     _lastNum = 1
-    nums.reverse()
-    for num in nums:
-        _lastNum *= num
+    _result_right = []
+    index = len(nums) - 1
+    while index >= 0:
+
+        _lastNum *= nums[index]
         _result_right.append(_lastNum)
+        index -= 1
 
     _result_right.reverse()
 
-    nums.reverse()
-
-    print("_result_left:  ", _result_left)
-    print("_result_right: ", _result_right)
+    print(_result_left)
+    print(_result_right)
 
     for index, num in enumerate(nums):
         if index == 0:
-            nums[index] = _result_right[0]
+            nums[index] = _result_right[1]
             continue
         
         if index == len(nums) - 1:
@@ -39,9 +44,7 @@ def productExceptSelf(nums: list) -> list:
 
 
 
-print("                [5, 4, 3, 2, 1]")
-nums = productExceptSelf([5, 4, 3, 2, 1])
-print("*"*10)
-print("Right answer:  ", [24, 30, 40, 60, 120])
-
-print("Calculated:    ", nums)
+nums = productExceptSelf([0])
+# nums = productExceptSelf([1, 2, 3, 4])
+# print([24, 30, 40, 60, 120])
+print(nums)
